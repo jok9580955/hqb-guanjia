7e32e2d703f38666a115f40cefcdc1d7f82352c4
liulei <jok9580955@gmail.com>
2026-03-19 16:31:48 +0800

    Initial Commit

---
+++ 华强北管家/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  华强北管家
+//
+//  Created by ll on 2026/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
